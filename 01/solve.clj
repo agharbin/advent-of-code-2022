5c3b08e4abe551ca8137fcfdc098163de5f093b5
Adam Harbin <harbin.adam@gmail.com>
2022-12-01 19:11:14 -0600

    01

---
+++ 01/solve.clj
@@ -0,0 +1,28 @@
+(ns advent.2022
+  (:require [clojure.string :as s]))
+
+(defn solve [input]
+  (->> input
+       (map #(apply + %))
+       sort
+       reverse
+       (take 3)
+       (apply +)))
+
+(defn parse-elf [input]
+  (->> input
+       s/split-lines
+       (map parse-long)))
+
+(defn parse-input [input]
+  (->> (s/split input #"\n\n")
+       (map parse-elf)))
+
+(defn solve-file [input]
+  (->> input
+       slurp
+       parse-input
+       solve
+       prn))
+
+(solve-file "input.dat")
