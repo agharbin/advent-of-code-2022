9ff039dc813081aa29cc6e09c20f600022eb9ea6
Adam Harbin <harbin.adam@gmail.com>
2023-05-05 20:24:45 -0500

    24

---
+++ 24/solve.clj
@@ -0,0 +1,91 @@
+(ns advent.2022.24
+  (:require
+    [clojure.string :as s]))
+
+;; Input Handling
+
+(defn parse-input [input]
+  (->> input
+       s/split-lines
+       (mapv vec)))
+
+(def grid (->> (slurp "input.dat") parse-input))
+(def grid-height (count grid))
+(def grid-width (count (first grid)))
+(def start-pos [0 1])
+(def end-pos [(dec grid-height) (- grid-width 2)])
+
+;; Blizzard Logic
+
+(def arrow->dir {\v [1 0] \^ [-1 0] \< [0 -1] \> [0 1]})
+
+(def blizzards
+  (->> (for [r (range grid-height) c (range grid-width)] [r c])
+       (filter #(#{\> \< \^ \v} (get-in grid %)))
+       (map (fn [x] {:loc x :dir (arrow->dir (get-in grid x))}))))
+
+(defn advance-blizzard-loc [{:keys [loc dir]} t]
+  (let [inner-height (- grid-height 2)
+        inner-width (- grid-width 2)
+        inner-loc (mapv + [-1 -1] loc) ; offset to region excluding walls
+        [r' c'] (mapv + inner-loc (mapv #(* % t) dir))
+        wrapped-r (mod r' inner-height)
+        wrapped-c (mod c' inner-width)]
+    (mapv + [1 1] [wrapped-r wrapped-c]))) ; offset back to 'real' coordinates
+
+(defn find-blizzard-locs-helper [t]
+  (->> blizzards
+       (map #(advance-blizzard-loc % t))
+       (into #{})))
+
+(def find-blizzard-locs (memoize find-blizzard-locs-helper))
+
+;; Navigation Logic
+
+(def wall-locs
+  (->> (for [r (range grid-height) c (range grid-width)] [r c])
+       (filter #(#{\#} (get-in grid %)))
+       (into #{})))
+
+(def not-wall? (complement wall-locs))
+(def all-moves #{[0 0] [1 0] [0 1] [-1 0] [0 -1]})
+
+(defn in-grid? [[r c]]
+  (and (<= 0 r)
+       (< r grid-height)
+       (<= 0 c)
+       (< c grid-width)))
+
+(defn possible-next-states [loc blizzard-locs]
+  (->> all-moves ; offsets from current position
+       (map #(mapv + loc %)) ; neighbor locations
+       (filter not-wall?)
+       (filter in-grid?)
+       (filter (complement blizzard-locs))))
+
+(defn bfs [start-state goal-state start-time]
+  (loop [q (conj clojure.lang.PersistentQueue/EMPTY [start-time start-state])
+         seen-states #{}]
+    (when (seq q)
+      (let [[dist state :as state-time] (peek q)]
+        (cond
+          (= goal-state state) dist
+          (seen-states state-time) (recur (pop q) seen-states)
+          :else
+            (let [candidates (possible-next-states state (find-blizzard-locs (inc dist)))]
+              (recur
+                (into (pop q) (for [s candidates] [(inc dist) s]))
+                (conj seen-states state-time))))))))
+
+;; Solutions
+
+(defn solve-1 []
+  (bfs start-pos end-pos 0))
+
+(defn solve-2 []
+  (let [time-after-first-leg (bfs start-pos end-pos 0)
+        time-after-second-leg (bfs end-pos start-pos time-after-first-leg)
+        time-after-third-leg (bfs start-pos end-pos time-after-second-leg)]
+    time-after-third-leg))
+
+(solve-1)
